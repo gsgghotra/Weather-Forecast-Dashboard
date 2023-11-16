771c648783a861176faec77a22f9ebf1ae077cdd
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-16 20:51:15 +0000

    Date added to the page. Script file connected

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Today's Date 
+var timeNow = dayjs();
+var formattedDate = dayjs(timeNow).format('dddd, MMMM D');
+
+console.log(formattedDate);
+
+//Append the time to the page
+var todayEl = document.getElementById('today');
+todayEl.innerText = formattedDate;
