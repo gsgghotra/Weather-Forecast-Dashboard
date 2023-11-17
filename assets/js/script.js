70ff70a291c601f99c5a6c4ea246a1ad1109f86f
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-17 16:44:20 +0000

    Fetch added, search event listener added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,30 @@ console.log(formattedDate);
 var todayEl = document.getElementById('todayDate');
 todayEl.innerText = formattedDate;
 
+
+//link generator
+function linkGenerator(){
+var url =  "https://api.openweathermap.org/data/2.5/weather?q=London,Burundi&appid=";
+    fetch(url)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
+
+//Location finder
+function locationFinder(){
+
+}
+
+//Plan
+//Search the location
+$('#search-button').on("click", function(event){
+    event.preventDefault();
+    console.log('Button Clicked');
+});
+
+//The location will 
