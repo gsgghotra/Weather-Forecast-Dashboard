beb8e22170660f11223c644d1ec3d95d302f5372
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-17 18:33:33 +0000

    Added Live search using dynamic array

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,10 @@ var todayEl = document.getElementById('todayDate');
 todayEl.innerText = formattedDate;
 
 
+//Variable for saving the saerch
+let searchVal;
 //link generator
-function linkGenerator(){
-var url =  "https://api.openweathermap.org/data/2.5/weather?q=London,Burundi&appid=";
-    fetch(url)
-    .then(function(response){
-        console.log(response);
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
-}
+
 
 //Location finder
 function locationFinder(){
@@ -28,10 +20,7 @@ function locationFinder(){
 }
 
 //Plan
-//Search the location
-$('#search-button').on("click", function(event){
-    event.preventDefault();
-    console.log('Button Clicked');
-});
 
-//The location will 
+
+//The location will be used to get lon and lat cordinates
+
