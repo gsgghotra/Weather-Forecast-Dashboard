bafeb3e170ece5f258572fa161a2d8f8ef0d73e2
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-17 09:41:32 +0000

    Search Icon added. FontAwesome CDN

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,20 @@ console.log(formattedDate);
 //Append the time to the page
 var todayEl = document.getElementById('today');
 todayEl.innerText = formattedDate;
+
+
+
+
+
+//Search bar border focus
+var search=document.getElementById('search');
+
+//On focus
+search.addEventListener('focus',(event)=>{
+    document.getElementById('search-wrapper').style.border="1px solid #1dbf73";
+});
+
+//on Focus out
+search.addEventListener('focusout',(event)=>{
+    document.getElementById('search-wrapper').style.border="1px solid rgba(0, 0, 0, 0.276)";
+});
