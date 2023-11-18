cfa6b25bb9b2e7dcabf70c29b8a0018062a22206
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-18 09:50:37 +0000

    Current weather fetch skeleton added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,32 @@ console.log(formattedDate);
 var todayEl = document.getElementById('todayDate');
 todayEl.innerText = formattedDate;
 
-//Plan
-
 
 //The location will be used to get lon and lat cordinates
 
+function urlGenerator (cityName, latitude, longitude){
+    //Generate the url for weather fetch
+    let baseURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+}
+
+function fetchWeather(){
+    fetch()
+    .then(function(response){
+        //Response
+    })
+    .then(function(data){
+        //Display Weather
+    })
+}
+
+function displayWeather(cityName, latitude, longitude){
+    console.log(cityName)
+    let searchedLocation = document.getElementById('searchedLocation');
+    searchedLocation.innerText = cityName;
+}
+
+
+urlGenerator('London City', 51.509865, -0.118092);
+
+
+//Planning
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,38 +1,26 @@
 //Search bar border focus
 var search=document.getElementById('search-input');
 let API_KEY = '';
-//On focus
-search.addEventListener('focus',(event)=>{
-    document.getElementById('search-form').style.border="1px solid #E01A59";
-});
-
-//on Focus out
-search.addEventListener('focusout',(event)=>{
-    document.getElementById('search-form').style.border="1px solid rgba(0, 0, 0, 0.276)";
-});
 
 //Variable for saving the saerch
 let searchVal;
+let cordinates = new Map();
 
 //Auto complete using jQuery with dynamic array
 var locations = [];
 $( "#search-input" ).autocomplete({
-source: function( request, response ) {
-        var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
-        response( $.grep( locations, function( item ){
-            return matcher.test( item );
-        }) );
-    }
+    source: locations
 });
 
 //Search the location and update the auto complete array on each key up
 $('#search-input').on("keyup", function(event){
+    //console.log(event)
     searchVal = $('#search-input').val();
     //if search exists and is greater than 3 characters
-    if (searchVal.length > 2){
-        console.log(searchVal);
+    if (searchVal.length > 1){
+        //console.log(searchVal);
         //pass the searched characters to the location finder function
-        autoCompleteGenerator(searchVal)
+        //autoCompleteGenerator(searchVal)
     }
     
 });
@@ -40,30 +28,48 @@ $('#search-input').on("keyup", function(event){
 //Auto complete Array generator and update search suggestion
 function autoCompleteGenerator(searchVal){
     var url =  "http://api.openweathermap.org/geo/1.0/direct?q="+searchVal+"&limit=5&appid="+API_KEY;
-        fetch(url)
-        .then(function(response){
-            //console.log(response);
-            return response.json();
-        })
-        .then(function(data){
-            //console.log(data)
-            for(let i = 0; i < data.length; i++){
-                //console.log(location.name+",", location.country);
-                var newLocation = data[i].name+","+ [data[i].country];
-                locations[i] = newLocation;
-                //console.log(locations)
-        }
+    fetch(url)
+    .then(function(response){
+        //console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        for(let i = 0; i < data.length; i++){
+            //console.log(location.name+",", location.country);
+            var newLocation = data[i].name+","+ [data[i].country];
+            cordinates.set(newLocation, {"lat":data[i].lat, "lon": data[i].lon})
+            locations[i] = newLocation;
+            //console.log(locations)
+            }
     })
 }
 
-//Search Button
-$('#search-button').on('click', (event)=>{
+//When clicked on the auto completion part
+$('#ui-id-1').on('click', (event)=>{
     event.preventDefault();
     //console.log(event);
-    searchVal = $('#search-input').val();
-    
-    //add saerched Location the screen
     let searchedLocation = document.getElementById('searchedLocation');
-    searchedLocation.innerText = searchVal;
+    const cityName = event.target.innerText;
 
+    //If map has the valuses of the selected city
+    if(cordinates.has(cityName)){
+        const locationLatitude = cordinates.get(cityName).lat;
+        const locationLongitude = cordinates.get(cityName).lon;
+        displayWeather(cityName, locationLatitude, locationLongitude);
+    }
+});
+
+
+//On focus add border
+search.addEventListener('focus',(event)=>{
+    document.getElementById('search-form').style.border="1px solid #E01A59";
+});
+//on Focus out remove border
+search.addEventListener('focusout',(event)=>{
+    document.getElementById('search-form').style.border="1px solid rgba(0, 0, 0, 0.276)";
+});
+//Show auto completion suggestion on click
+search.addEventListener('click',()=>{
+    document.getElementById('ui-id-1').style.display="block";
 })
